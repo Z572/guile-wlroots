6d6f9109c56259e98e87610ecaab5549807dc36b
Z572 <873216071@qq.com>
2022-08-18 09:48:17 +0800

    input-device.scm: add wlr-input-device-type procedure and enums
    wlroots/types/input-device.scm:
        add wlr-input-device-type, wlr-input-device-type->value,
        value->wlr-input-device-type procedure,
        add WLR_INPUT_DEVICE_KEYBOARD
        WLR_INPUT_DEVICE_POINTER
        WLR_INPUT_DEVICE_TOUCH
        WLR_INPUT_DEVICE_TABLET_TOOL
        WLR_INPUT_DEVICE_TABLET_PAD
        WLR_INPUT_DEVICE_SWITCH value.

--- wlroots/types/input-device.scm
+++ wlroots/types/input-device.scm
@@ -7,9 +7,27 @@
   #:export (%wlr-input-device-struct
             wrap-wlr-input-device
             unwrap-wlr-input-device
-            wlr-input-device-name))
+            wlr-input-device-name
+            WLR_INPUT_DEVICE_KEYBOARD
+            WLR_INPUT_DEVICE_POINTER
+            WLR_INPUT_DEVICE_TOUCH
+            WLR_INPUT_DEVICE_TABLET_TOOL
+            WLR_INPUT_DEVICE_TABLET_PAD
+            WLR_INPUT_DEVICE_SWITCH
+            wlr-input-device-type
+            wlr-input-device-type->value
+            value->wlr-input-device-type))
 
 (define-wlr-types-class wlr-input-device)
+(define-enumeration
+  value->wlr-input-device-type
+  wlr-input-device-type->value
+  (WLR_INPUT_DEVICE_KEYBOARD 0)
+  (WLR_INPUT_DEVICE_POINTER 1)
+  (WLR_INPUT_DEVICE_TOUCH 2)
+  (WLR_INPUT_DEVICE_TABLET_TOOL 3)
+  (WLR_INPUT_DEVICE_TABLET_PAD 4)
+  (WLR_INPUT_DEVICE_SWITCH 5))
 (define %wlr-input-device-struct
   (bs:struct `((impl ,(bs:pointer '*))
                (type ,int)
@@ -32,3 +50,10 @@
   (bytestructure-ref
    (pointer->bytestructure (unwrap-wlr-input-device device) %wlr-input-device-struct)
    'name))
+
+(define (wlr-input-device-type device)
+  (bytestructure-ref
+   (pointer->bytestructure
+    (unwrap-wlr-input-device device)
+    %wlr-input-device-struct)
+   'type))

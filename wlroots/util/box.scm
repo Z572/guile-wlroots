cc2008540a3df56397b75742ec7be9997d6ea41e
Z572 <873216071@qq.com>
2022-08-25 21:42:01 +0800

    u

--- wlroots/util/box.scm
+++ wlroots/util/box.scm
@@ -1,6 +1,8 @@
 (define-module (wlroots util box)
+  #:use-module (wlroots types)
+  #:use-module (wayland util)
   #:use-module (bytestructures guile)
-  #:export (%wlr-box-struct %wlr-fbox-struct))
+  #:export (%wlr-box-struct %wlr-fbox-struct <wlr-box> make-wlr-box list->wlr-box))
 (define %wlr-box-struct
   (bs:struct `((x ,int) (y ,int) (width ,int) (height ,int))))
 
@@ -9,3 +11,16 @@
                (y ,double)
                (width ,double)
                (height ,double))))
+
+(define-wlr-types-class-public
+  wlr-box)
+(define (make-wlr-box x y width height)
+  (wrap-wlr-box (bytestructure->pointer
+                 (bytestructure
+                  %wlr-box-struct
+                  `((x ,x)
+                    (y ,y)
+                    (width ,width)
+                    (height ,height))))))
+(define (list->wlr-box l)
+  (apply make-wlr-box l))

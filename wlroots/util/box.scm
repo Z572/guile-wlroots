52e0191c9ccec6dce84091bfb90754ebcc9415cb
Z572 <873216071@qq.com>
2022-11-04 23:22:40 +0800

    wlroots/util/box.scm: (<wlr-box>): add shallow-clone method .

--- wlroots/util/box.scm
+++ wlroots/util/box.scm
@@ -47,6 +47,9 @@
                     (y ,y)
                     (width ,width)
                     (height ,height))))))
+
+(define-method (shallow-clone (box <wlr-box>))
+  (make-wlr-box (box-x box) (box-y box) (box-width box) (box-height box)))
 (define (list->wlr-box l)
   (apply make-wlr-box l))
 

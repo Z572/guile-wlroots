047869e9631dfac4375cb197e898e004cf60f4c5
Z572 <873216071@qq.com>
2022-11-28 21:38:06 +0800

    wlroots/types/buffer.scm: add wlr-buffer-{drop,{,un}lock} procedure.

--- wlroots/types/buffer.scm
+++ wlroots/types/buffer.scm
@@ -4,6 +4,20 @@
   #:use-module (wlroots utils)
   #:use-module ((system foreign) #:prefix ffi:)
   #:export (wrap-wlr-buffer
-            unwrap-wlr-buffer))
+            unwrap-wlr-buffer
+            wlr-buffer-drop
+            wlr-buffer-lock
+            wlr-buffer-unlock))
 
 (define-wlr-types-class wlr-buffer)
+
+(define-wlr-procedure (wlr-buffer-drop buffer)
+  (ffi:void "wlr_buffer_drop" '(*))
+  (% unwrap-wlr-buffer buffer))
+
+(define-wlr-procedure (wlr-buffer-lock buffer)
+  ('* "wlr_buffer_lock" '(*))
+  (wrap-wlr-buffer (% unwrap-wlr-buffer buffer)))
+(define-wlr-procedure (wlr-buffer-unlock buffer)
+  (ffi:void "wlr_buffer_unlock" '(*))
+  (% unwrap-wlr-buffer buffer))

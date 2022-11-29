bb6d2e54f1cd6c5476f80820d6548427e0088554
Z572 <873216071@qq.com>
2022-11-29 23:04:35 +0800

    wlroots/types/idle.scm: add wlr-idle-create procedure.

--- wlroots/types/idle.scm
+++ wlroots/types/idle.scm
@@ -1,14 +1,20 @@
 (define-module (wlroots types idle)
   #:use-module (wlroots utils)
+  #:use-module (wayland display)
   #:use-module ((system foreign) #:prefix ffi:)
   #:use-module (wlroots types seat)
   #:use-module (wlroots types)
   #:export (<wlr-idle>
+            wlr-idle-create
             wlr-idle-notify-activity
             wrap-wlr-idle
             unwrap-wlr-idle))
 
 (define-wlr-types-class wlr-idle)
+
+(define-wlr-procedure (wlr-idle-create display)
+  ('* "wlr_idle_create" '(*))
+  (wrap-wlr-idle (% (unwrap-wl-display display))))
 
 (define-wlr-procedure (wlr-idle-notify-activity idle seat)
   (ffi:void "wlr_idle_notify_activity" '(* *))

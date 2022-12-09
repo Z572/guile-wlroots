00dabaacc1ab0f12da0e13ed499075570bf1eb45
Z572 <873216071@qq.com>
2022-12-09 12:44:19 +0800

    wlroots/types/output-management.scm: add wlr-output-manager-v1-create procedure.

--- wlroots/types/output-management.scm
+++ wlroots/types/output-management.scm
@@ -1,12 +1,20 @@
 (define-module (wlroots types output-management)
+  #:use-module (wayland display)
   #:use-module (wlroots types)
+  #:use-module (wlroots utils)
   #:export (<wlr-output-manager-v1>
             wrap-wlr-output-manager-v1
             unwrap-wlr-output-manager-v1
 
             <wlr-output-configuration-v1>
             wrap-wlr-output-configuration-v1
-            unwrap-wlr-output-configuration-v1))
+            unwrap-wlr-output-configuration-v1
+
+            wlr-output-manager-v1-create))
 
 (define-wlr-types-class wlr-output-manager-v1)
 (define-wlr-types-class wlr-output-configuration-v1)
+
+(define-wlr-procedure (wlr-output-manager-v1-create display)
+  ('* "wlr_output_manager_v1_create" (list '*))
+  (wrap-wlr-output-manager-v1 (% (unwrap-wl-display display))))

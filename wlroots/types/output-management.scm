1f0ddecfe361ea2aaa937f4c379050f3d3622d64
Z572 <873216071@qq.com>
2022-12-19 17:29:44 +0800

    wlroots/types/output-management.scm: add %wlr-output-manager-v1-struct.

--- wlroots/types/output-management.scm
+++ wlroots/types/output-management.scm
@@ -1,8 +1,14 @@
 (define-module (wlroots types output-management)
   #:use-module (wayland display)
+  #:use-module (wayland list)
+  #:use-module (wayland listener)
+  #:use-module (wayland signal)
+  #:use-module (wayland util)
   #:use-module (wlroots types)
   #:use-module (wlroots utils)
-  #:export (<wlr-output-manager-v1>
+  #:use-module (bytestructures guile)
+  #:export (%wlr-output-manager-v1-struct
+            <wlr-output-manager-v1>
             wrap-wlr-output-manager-v1
             unwrap-wlr-output-manager-v1
 
@@ -11,6 +17,20 @@
             unwrap-wlr-output-configuration-v1
 
             wlr-output-manager-v1-create))
+
+(define %wlr-output-manager-v1-struct
+  (bs:struct `((display ,(bs:pointer '*))
+               (global ,(bs:pointer '*))
+               (resources ,%wl-list)
+               (heads ,%wl-list)
+               (serial ,uint32)
+               (current-configuration-dirty ,int8) ;; bool
+               (events ,(bs:struct `((apply ,%wl-signal-struct)
+                                     (test ,%wl-signal-struct)
+                                     (destroy ,%wl-signal-struct))))
+               (display-destroy ,%wl-listener)
+               (data ,(bs:pointer 'void)))))
+
 
 (define-wlr-types-class wlr-output-manager-v1)
 (define-wlr-types-class wlr-output-configuration-v1)

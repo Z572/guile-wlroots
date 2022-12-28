635db52552716599a0086edb54f37e73b8cb5324
Z572 <873216071@qq.com>
2022-12-28 16:43:20 +0800

    wlroots/types/data-device.scm: improve <wlr-drag>.

--- wlroots/types/data-device.scm
+++ wlroots/types/data-device.scm
@@ -15,13 +15,16 @@
   #:use-module (wlroots types surface)
   #:use-module (wlroots utils)
   #:use-module (oop goops)
-  #:export (<wlr-drop-icon>
+  #:export (<wlr-drag>
+            <wlr-drop-icon>
             wrap-wlr-data-source
             unwrap-wlr-data-source
             wrap-wlr-data-device-manager
             unwrap-wlr-data-device-manager
             wrap-wlr-drap-icon
             unwrap-wlr-drap-icon
+            wrap-wlr-drag
+            unwrap-wlr-drag
             wlr-data-device-manager-create
             %wlr-drag-struct
             %wlr-data-source-struct
@@ -29,7 +32,23 @@
             .drag
             .surface
             .mapped
-            .data))
+            .data
+            .grab-type
+            .keyboard-grab
+            .pointer-grab
+            .touch-grab
+            .seat
+            .seat-client
+            .focus-client
+            .icon
+            .focus
+            .source
+            .started
+            .dropped
+            .cancelling
+            .grab-touch-id
+            .touch-id
+            ))
 
 (eval-when (expand load eval)
   (define %wlr-data-source-struct
@@ -75,6 +94,22 @@
                  (data ,(bs:pointer 'void))))))
 
 (define-wlr-types-class wlr-drag ()
+  (grab-type      #:allocation #:bytestructure #:accessor .grab-type    )
+  (keyboard-grab  #:allocation #:bytestructure #:accessor .keyboard-grab)
+  (pointer-grab   #:allocation #:bytestructure #:accessor .pointer-grab )
+  (touch-grab     #:allocation #:bytestructure #:accessor .touch-grab   )
+  (seat           #:allocation #:bytestructure #:accessor .seat         )
+  (seat-client    #:allocation #:bytestructure #:accessor .seat-client  )
+  (focus-client   #:allocation #:bytestructure #:accessor .focus-client )
+  (icon           #:allocation #:bytestructure #:accessor .icon         )
+  (focus          #:allocation #:bytestructure #:accessor .focus        )
+  (source         #:allocation #:bytestructure #:accessor .source       )
+  (started        #:allocation #:bytestructure #:accessor .started      )
+  (dropped        #:allocation #:bytestructure #:accessor .dropped      )
+  (cancelling     #:allocation #:bytestructure #:accessor .cancelling   )
+  (grab-touch-id  #:allocation #:bytestructure #:accessor .grab-touch-id)
+  (touch-id       #:allocation #:bytestructure #:accessor .touch-id     )
+  (data           #:allocation #:bytestructure #:accessor .data         )
   #:descriptor %wlr-drag-struct)
 
 (define-wlr-types-class wlr-data-source ()

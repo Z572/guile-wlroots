f35d135ffbc7d4590a85c75c6872cb89252a42c0
Z572 <873216071@qq.com>
2023-01-20 21:08:42 +0800

    wlroots/util/box.scm: <wlr-box>: improve.

--- wlroots/util/box.scm
+++ wlroots/util/box.scm
@@ -10,23 +10,20 @@
 
 (define-bytestructure-class <wlr-box> (<box>)
   %wlr-box-struct wrap-wlr-box unwrap-wlr-box wlr-box?
-  (x #:accessor box-x)
-  (y #:accessor box-y)
-  (height #:accessor box-height)
-  (width #:accessor box-width))
+  (x #:init-keyword #:x #:accessor box-x)
+  (y #:init-keyword #:y #:accessor box-y)
+  (width #:init-keyword #:width #:accessor box-width)
+  (height #:init-keyword #:height #:accessor box-height))
 
 (define (make-wlr-box x y width height)
-  (wrap-wlr-box (bytestructure
-                 %wlr-box-struct
-                 `((x ,x)
-                   (y ,y)
-                   (width ,width)
-                   (height ,height)))))
+  (make <wlr-box>
+    #:x x
+    #:y y
+    #:width width
+    #:height height))
 
 (define-method (shallow-clone (box <wlr-box>))
   (make-wlr-box (box-x box) (box-y box) (box-width box) (box-height box)))
+
 (define (list->wlr-box l)
   (apply make-wlr-box l))
-
-(define-method (->bytestructure (box <wlr-box>))
-  (pointer->bytestructure (get-pointer box) %wlr-box-struct))

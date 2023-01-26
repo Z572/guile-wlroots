a68019acf975deb2263e749757214ed60491ba54
Z572 <873216071@qq.com>
2023-01-26 15:30:11 +0800

    wlroots/types/compositor.scm: add wlr-surface-is-subsurface procedure.

--- wlroots/types/compositor.scm
+++ wlroots/types/compositor.scm
@@ -2,11 +2,14 @@
   #:use-module (wayland display)
   #:use-module (wlroots render renderer)
   #:use-module (wlroots types)
+  #:use-module (wlroots types surface)
   #:use-module (wlroots utils)
+  #:use-module ((system foreign) #:prefix ffi:)
   #:use-module (oop goops)
   #:export (wrap-wlr-compositor
             unwrap-wlr-compositor
-            wlr-compositor-create))
+            wlr-compositor-create
+            wlr-surface-is-subsurface))
 
 (define-wlr-types-class wlr-compositor ()
   #:descriptor %wlr-compositor-struct)
@@ -16,3 +19,7 @@
   (wrap-wlr-compositor
    (% (unwrap-wl-display display)
       (unwrap-wlr-renderer renderer))))
+
+(define-wlr-procedure (wlr-surface-is-subsurface surface)
+  (ffi:int8 "wlr_surface_is_subsurface" '(*))
+  (not (zero? (% (unwrap-wlr-surface surface)))))

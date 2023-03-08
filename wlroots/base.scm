d5204c9144b8dffbb55c04439a0613853872b11a
Z572 <873216071@qq.com>
2023-03-08 23:12:19 +0800

    remove wlroots/base.scm.

--- wlroots/base.scm
+++
@@ -1,9 +0,0 @@
-(define-class <wlr> ())
-(define-class <wlr-pointer> (<wlr>)
-  (pointer #:accessor .pointer #:init-keyword #:pointer))
-
-;; (define-method (= (a <class>) (b <class>)))
-(define-method (= (f <wlr-pointer>) (l <wlr-pointer>))
-  (and (eq (class-of f) (class-of l))
-       (= (pointer-address (.pointer f))
-          (pointer-address (.pointer l)))))

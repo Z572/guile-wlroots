8e197e20d8a71ab7bad1ccc2e9c0c84207d8db4d
Z572 <zhengjunjie@iscas.ac.cn>
2023-04-27 11:27:06 +0800

    update guix.scm

--- guix.scm
+++ guix.scm
@@ -20,96 +20,7 @@
  (guix gexp)
  (guix git-download)
  (guix packages)
- (guix utils)
- )
-(define libdrm-next
-  (package
-    (inherit libdrm)
-    (name "libdrm")
-    (version "2.4.114")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://gitlab.freedesktop.org/mesa/drm")
-             (commit (string-append "libdrm-" version))))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "1vxfha0anvxf2i1lirh0m35avkv9rdmhd5c3s6fp6g432963vgrh"))))))
-(define wayland-next
-  (package
-    (inherit wayland)
-    (name "wayland")
-    (version "1.21.0")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://gitlab.freedesktop.org/wayland/wayland")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "0fwad6w5jm32c04wh4gca7d1ixdj4b9dnsiy1h6qd9nxs0w47wwy"))))))
-(define wayland-protocols-next
-  (package
-    (inherit wayland-protocols)
-    (name "wayland-protocols")
-    (version "1.31")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://gitlab.freedesktop.org/wayland/wayland-protocols")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "0kpyvnzlwfj9d57v43z5fhk7fliz6224m4hw1xj425c8vrjbw0nx"))))
-    (inputs
-     (modify-inputs (package-inputs wayland-protocols)
-                    (replace "wayland" wayland-next)))))
-(define libinput-next
-  (package
-    (inherit libinput)
-    (version "1.19.4")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append "https://freedesktop.org/software/libinput/"
-                                  "libinput-" version ".tar.xz"))
-              (sha256
-               (base32
-                "0h5lz54rrl48bhi3vki6s08m6rn2h62rlf08dhgchdm9nmqaaczz"))))))
-(define wlroots-next
-  (package
-    (inherit wlroots)
-    (name "wlroots")
-    (version "0.16.0")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://gitlab.freedesktop.org/wlroots/wlroots")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "18rfr3wfm61dv9w8m4xjz4gzq2v3k5vx35ymbi1cggkgbk3lbc4k"))))
-    (arguments (substitute-keyword-arguments (package-arguments wlroots)
-                 ((#:configure-flags flags ''())
-                  `(cons "-Dbackends=['drm','libinput','x11']" ,flags))
-                 ((#:phases phases)
-                  #~(modify-phases #$phases
-                      (add-before 'configure 'sub-hwdata
-                        (lambda* (#:key native-inputs inputs #:allow-other-keys)
-                          (substitute* "backend/drm/meson.build"
-                            (("/usr/share/hwdata/pnp.ids")
-                             (search-input-file (or native-inputs inputs)
-                                                "share/hwdata/pnp.ids")))))))))
-
-    (propagated-inputs
-     (modify-inputs (package-propagated-inputs wlroots)
-                    (prepend (list hwdata "pnp") libdrm-next libglvnd xcb-util-renderutil)
-                    (replace "wayland" wayland-next)
-                    (replace "libinput-minimal" libinput-next)
-                    (replace "wayland-protocols" wayland-protocols-next)))))
+ (guix utils))
 
 (define guile-wlroots
   (package
@@ -143,7 +54,7 @@
            pkg-config
            texinfo
            guile-3.0-latest))
-    (inputs (list guile-3.0-latest wlroots-next))
+    (inputs (list guile-3.0-latest wlroots))
     (propagated-inputs
      (list guile-bytestructures
            (primitive-load
@@ -165,7 +76,5 @@
     (description "")
     (home-page "")
     (license license:gpl3+)))
+
 guile-wlroots
-;; (list
-;;  info-reader
-;;  bear)

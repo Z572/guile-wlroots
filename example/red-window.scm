3be739b9e41603fcf4abc440d6cbf61083620ba9
Z572 <zhengjunjie@iscas.ac.cn>
2023-05-31 17:48:33 +0800

    add example/red-window.scm.

---
+++ example/red-window.scm
@@ -0,0 +1,47 @@
+(use-modules
+ (wayland)
+ (util572 color)
+ (wlroots))
+
+;;; this a simple example, can not input, display cursor , display client.
+;;; please don't try at tty. use x11 or wayland backend. it just display a
+;;; red windows.
+
+(define w-display (wl-display-create))
+(define w-backend (wlr-backend-autocreate w-display))
+(define w-renderer (wlr-renderer-autocreate w-backend))
+(define w-allocator (wlr-allocator-autocreate w-backend w-renderer))
+
+(define output-frame-listener
+  (make-wl-listener
+   (lambda (listener data)
+     (let ((output (wrap-wlr-output data)))
+       (wlr-output-attach-render output #f)
+       (wlr-renderer-begin w-renderer (.width output) (.height output))
+       (wlr-renderer-clear w-renderer (make-rgba-color "#f000"))
+       (wlr-renderer-end w-renderer)
+       (wlr-output-commit output)))))
+
+(define w-backend-new-output-listener
+  (make-wl-listener
+   (lambda (listener data)
+     (let* ((output (wrap-wlr-output data)))
+       (display "I get new output!\n")
+       (wlr-output-init-render
+        output w-allocator w-renderer)
+       (wl-signal-add (get-event-signal output 'frame) output-frame-listener)
+       (wlr-output-commit output)))))
+
+(wlr-renderer-init-wl-display w-renderer w-display)
+
+(wl-signal-add (get-event-signal w-backend 'new-output)
+               w-backend-new-output-listener)
+
+(wlr-backend-start w-backend)
+
+(setenv "WAYLAND_DISPLAY" (wl-display-add-socket-auto w-display))
+
+(wl-display-run w-display)
+
+(wl-display-destroy-clients w-display)
+(wl-display-destroy w-display)
